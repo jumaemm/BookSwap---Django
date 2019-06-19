1514550386a46d9c881cd19c57914771f7b2cd0a
hazy254 <jumaemmanuel.EJ@outlook.com>
2019-06-19 14:52:46 +0300

    "Templates set up and attempting image upload"

--- bookswap/books/urls.py
+++ bookswap/books/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.BookListView.as_view(), name = "book_list")
+    path("", views.BookListView.as_view(), name = "book_list"),
+    path("books/<int:pk>/", views.BookDetailView.as_view(), name = "book_detail"),
+    path("books/create_book/", views.CreateBookView.as_view(), name = "create_book")
 ]
--- bookswap/books/views.py
+++ bookswap/books/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView, DetailView, CreateView)
 from .models import Book
 from .forms import BookForm
 from django.utils import timezone
+from django.contrib import auth
 # Create your views here.
 class BookListView(ListView):
     model = Book
-
+    context_object_name = 'book_list'
     def get_queryset(self):
         return Book.objects.filter(posted_date__lte = timezone.now()).order_by('posted_date')
 
 class BookDetailView(DetailView):
     model = Book
 
-class CreateBookView(CreateView):
+
+class CreateBookView(LoginRequiredMixin, CreateView):
     form_class = BookForm
     model = Book
+    success_url = "book_list"
+    template_name = "book_form.html"
+
+    def form_valid(self, form):
+        print("This is the current user:" +  str(self.request.user.id))
+        form.instance.owner = auth.get_user(self.request)
+        book_post(self.request, pk = form.instance.id)
+        return super().form_valid(form)
 
 
 def book_post(request, pk):
